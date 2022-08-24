7c254e93521fb547e67890021a2a521a0c3892c1
Dasavanth-Alapati <dasavanth.work@gmail.com>
2022-08-24 17:57:26 +0530

    changed index to show feed

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import Feed from './components/feed/feed';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -12,7 +14,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Feed />
     </Provider>
   </React.StrictMode>
 );
