6d66c384b92cae9272513128f40c0d21e776e16e
Dasavanth-Alapati <dasavanth.work@gmail.com>
2022-08-24 18:34:33 +0530

    formatted

--- src/components/feed/feed.js
+++ src/components/feed/feed.js
@@ -4,26 +4,25 @@ import { fetchfeed } from '../../api/api';
 import Post from './post';
 
 class Feed extends Component {
-  state = { feed:null }
+  state = { feed: null }
   componentDidMount() {
     fetchfeed().then(res => {
-    this.setState({feed:res.data});
+      this.setState({ feed: res.data });
     })
-    
   }
 
-  render() { 
+  render() {
     let posts = []
-    if (this.state.feed !== null){
+    if (this.state.feed !== null) {
       for (let i = 0; i < this.state.feed.length; i++) {
-      const element = this.state.feed[i];
-      posts.push(<Post key = {element.id} post = {element} />)
+        const element = this.state.feed[i];
+        posts.push(<Post key={element.id} post={element} />)
       }
-    return (<Container className= "d-flex justify-content-center"><div className='mt-3'>{posts}</div></Container>);
+      return (<Container className="d-flex justify-content-center"><div className='mt-3'>{posts}</div></Container>);
     }
     else return (<div>Loading...</div>)
 
   }
 }
- 
+
 export default Feed;
