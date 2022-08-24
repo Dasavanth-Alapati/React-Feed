849705e7559840c46baf6b2e9bc2376082635905
Dasavanth-Alapati <dasavanth.work@gmail.com>
2022-08-24 18:07:14 +0530

    removed an unused span

--- src/components/feed/post.js
+++ src/components/feed/post.js
@@ -12,7 +12,7 @@ function Post(props) {
         <Card.Text>
           {props.post.content}
         </Card.Text>
-        <Card.Text><small span>{props.post.profileid.credid.username}</small></Card.Text>
+        <Card.Text><small>{props.post.profileid.credid.username}</small></Card.Text>
         <Interactor></Interactor>
       </Card.Body>
       <Card.Footer className="text-muted">{moment(props.post.created_at).fromNow()}</Card.Footer>
