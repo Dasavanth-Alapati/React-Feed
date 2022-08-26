b9ab7afe70f0707b2aa2c661150469b18204dd95
Dasavanth-Alapati <dasavanth.work@gmail.com>
2022-08-26 18:16:00 +0530

    login api

--- src/api/api.js
+++ src/api/api.js
@@ -3,3 +3,7 @@ const baseurl = 'http://localhost:8000/';
 
 export async function fetchfeed(){
 return await axios.get(baseurl + 'posts/apifeed/');}
+
+export async function login(credentials) {
+    return await axios.post(baseurl + 'api/token/', credentials);
+}
