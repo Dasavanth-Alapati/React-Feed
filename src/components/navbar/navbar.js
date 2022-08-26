4fa9b7b8f8600b28134e2107b26f1388e0c1973d
Dasavanth-Alapati <dasavanth.work@gmail.com>
2022-08-26 17:31:40 +0530

    router links in nav bar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,21 +3,24 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import React, { Component } from 'react';
 import logo from '../../favicon.ico';
+import { Link } from "react-router-dom";
+
 
 class NavBar extends Component {
 
     state = {  }
     render() { 
-        return ( <Navbar bg="primary">
+        return ( 
+        <Navbar bg="primary">
         <Container>
-          <img src={logo} width="30" height="30" alt="logo" />
-          <Navbar.Brand href="#home" className='text-light'>KisanKiosk</Navbar.Brand>
+          <Navbar.Brand as={Link} to='/' className='text-light'><img src={logo} width="30" height="30" alt="logo" />KisanKiosk</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
-              <Nav.Link href="#profile" className='text-light'>Profile</Nav.Link>
+              <Nav.Link as={Link} to='login' className='text-light'>Sign In</Nav.Link>
           </Navbar.Collapse>
         </Container>
-      </Navbar> );
+      </Navbar>
+      );
     }
 }
  
