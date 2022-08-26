c44940f5bff4656fbaa9328fdcdb03ec8c27956a
Dasavanth-Alapati <dasavanth.work@gmail.com>
2022-08-26 17:31:10 +0530

    basic routing using react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import  Feed  from './components/feed/feed';
+import Login from './components/login/login';
 import NavBar from './components/navbar/navbar';
-
+import {Routes,Route } from "react-router-dom";
 function App() {
   return (
+
     <div className="App">
       <NavBar/>
-      <Feed/>
+      <Routes>
+      <Route path='/' element={<>Welcome</>}/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/Feed' element={<Feed />}/>
+      </Routes>
     </div>
+
   );
 }
 
