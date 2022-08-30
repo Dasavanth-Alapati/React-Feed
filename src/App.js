19557d17da941dd319d4da753919b9bbacdd0d16
Dasavanth-Alapati <dasavanth.work@gmail.com>
2022-08-30 16:39:05 +0530

    added a better home page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import  Feed  from './components/feed/feed';
 import Login from './components/login/login';
 import NavBar from './components/navbar/navbar';
-import {Routes,Route } from "react-router-dom";
+import {Routes,Route, Link } from "react-router-dom";
+import { Container } from 'react-bootstrap';
 function App() {
   return (
 
     <div className="App">
       <NavBar/>
       <Routes>
-      <Route path='/' element={<>Welcome</>}/>
+      <Route path='/' element={<Container>Welcome, please <Link to='/login'>Signin</Link> to view feed</Container>}/>
       <Route path='/login' element={<Login />}/>
       <Route path='/Feed' element={<Feed />}/>
       </Routes>
