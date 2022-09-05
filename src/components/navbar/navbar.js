a01ea8027177b7c44f9b24e1c531abb1cdea0011
Dasavanth-Alapati <dasavanth.work@gmail.com>
2022-09-05 12:54:43 +0530

    switched navbar into a function

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,26 +1,21 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import React, { Component } from 'react';
 import logo from '../../favicon.ico';
 import { Link } from "react-router-dom";
 
 
-class NavBar extends Component {
+const NavBar = (props) => {
+    
 
-    state = {  }
-    render() { 
-        return ( 
+    return (
         <Navbar className="px-3" bg="primary">
-        
-          <Navbar.Brand as={Link} to='/' className='text-light'><img src={logo} width="30" height="30" alt="logo" />KisanKiosk</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
-              <Nav.Link as={Link} to='login' className='text-light'>Sign In</Nav.Link>
-          </Navbar.Collapse>
-        
-      </Navbar>
-      );
-    }
+            <Navbar.Brand as={Link} to='/' className='text-light'><img src={logo} width="30" height="30" alt="logo" />KisanKiosk</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
+                <Nav.Link as={Link} to='login' className='text-light'>Sign In</Nav.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    );
 }
- 
+
 export default NavBar;
