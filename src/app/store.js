018f3faa91cbe771ef6a9af0c64478694788d2fd
Dasavanth-Alapati <dasavanth.work@gmail.com>
2022-09-06 11:58:29 +0530

    login slice added to global store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import loginReducer from '../components/login/loginSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    profile: loginReducer,
   },
 });
