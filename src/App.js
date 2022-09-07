8478b8997d6bd565572d8a6cbf0da9c24307de01
Dasavanth-Alapati <dasavanth.work@gmail.com>
2022-09-07 16:22:33 +0530

    using store to keep profile data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import  Feed  from './components/feed';
 import Login from './components/login';
 import NavBar from './components/navbar';
 import {Routes,Route, Link } from "react-router-dom";
 import { Container } from 'react-bootstrap';
+import { isLoggedIn } from 'axios-jwt';
+import { useDispatch } from 'react-redux';
+import { fetchProfile } from './services/api';
+import { setProfile } from './redux/slices/profileSlice';
+
+
 function App() {
+
+  const [loggedIn,setLoggedIn] = useState(isLoggedIn());
+  const dispatch = useDispatch();
+  if (loggedIn)
+    fetchProfile().then(res => {
+      dispatch(setProfile(res.data));
+    })
   return (
 
     <div className="App">
-      <NavBar/>
+      <NavBar loggedIn = {loggedIn} setLoggedIn = {setLoggedIn}/>
       <Routes>
       <Route path='/' element={<Container>Welcome, please <Link to='/login'>Signin</Link> to view feed</Container>}/>
-      <Route path='/login' element={<Login />}/>
-      <Route path='/Feed' element={<Feed />}/>
+      <Route path='/login' element={<Login setLoggedIn = {setLoggedIn}/>}/>
+      <Route path='/Feed' element={<Feed/>}/>
       </Routes>
     </div>
 
