7215fb0a44cf203ac26fd297c234db3f72d3ac7a
Dasavanth-Alapati <dasavanth.work@gmail.com>
2022-09-08 16:39:55 +0530

    shifted like n dislike to functions, prep for backend support

--- src/components/interactor.js
+++ src/components/interactor.js
@@ -1,24 +1,72 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThumbsUp,faThumbsDown } from '@fortawesome/free-solid-svg-icons';
-import { faThumbsUp as rfaThumbsUp,faThumbsDown as rfaThumbsDown } from '@fortawesome/free-regular-svg-icons';
+import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp as rfaThumbsUp, faThumbsDown as rfaThumbsDown } from '@fortawesome/free-regular-svg-icons';
 import Button from 'react-bootstrap/Button';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
-class Interactor extends Component {
-    state = {
-        like: rfaThumbsUp,
-        dislike: rfaThumbsDown,
-        likecount: this.props.like.like,
-        dislikecount: this.props.like.dislike,
+
+const Interactor = (props) => {
+    const id = useSelector(state => state.profile.data.id);
+    const checkLike = (likeinfo) => {
+        for (let i = 0; i < likeinfo.length; i++) {
+            const element = likeinfo[i];
+            if (element.profileid.id === id && element.likes === 'YES') return faThumbsUp;
+        }
+        return rfaThumbsUp;
+    };
+    const checkDislike = (likeinfo) => {
+        for (let i = 0; i < likeinfo.length; i++) {
+            const element = likeinfo[i];
+            if (element.profileid.id === id && element.likes === 'NO') return faThumbsDown;
+        }
+        return rfaThumbsDown;
+    };
+    const [state, setState] = useState({
+        like: checkLike(props.like.likeinfo),
+        dislike: checkDislike(props.like.likeinfo),
+        likecount: props.like.like,
+        dislikecount: props.like.dislike,
+    });
+    const like = () => {
+        let sudoState = { ...state };
+        if (sudoState.like === rfaThumbsUp) {
+            sudoState.like = faThumbsUp;
+            sudoState.likecount += 1;
+            if (sudoState.dislike === faThumbsDown) {
+                sudoState.dislike = rfaThumbsDown;
+                sudoState.dislikecount -= 1;
+            }
+        }
+        else {
+            sudoState.like = rfaThumbsUp;
+            sudoState.likecount -= 1;
+        }
+        setState(sudoState);
+    };
+    const dislike = () => {
+        let sudoState = { ...state };
+        if (sudoState.dislike === rfaThumbsDown) {
+            sudoState.dislike = faThumbsDown;
+            sudoState.dislikecount += 1;
+            if (sudoState.like === faThumbsUp) {
+                sudoState.like = rfaThumbsUp;
+                sudoState.likecount -= 1;
+            }
+        }
+        else {
+            sudoState.dislike = rfaThumbsDown;
+            sudoState.dislikecount -= 1;
+        }
+        setState(sudoState);
     };
 
-    render() {
-        return (<div className="d-flex justify-content-between">
-            <Button variant="primary" onClick={() => this.setState(this.state.like === rfaThumbsUp ? (this.state.dislike === faThumbsDown ? { like: faThumbsUp, dislike: rfaThumbsDown, likecount: this.state.likecount + 1, dislikecount: this.state.dislikecount - 1 } : { like: faThumbsUp, likecount: this.state.likecount + 1 }) : { like: rfaThumbsUp, likecount: this.state.likecount - 1 })}><FontAwesomeIcon icon={this.state.like} /> {this.state.likecount}</Button>
-            <Button variant="primary" onClick={() => this.setState(this.state.dislike === rfaThumbsDown ? (this.state.like === faThumbsUp ? { like: rfaThumbsUp, dislike: faThumbsDown, likecount: this.state.likecount - 1, dislikecount: this.state.dislikecount + 1 } : { dislike: faThumbsDown, dislikecount: this.state.dislikecount + 1 }) : { dislike: rfaThumbsDown, dislikecount: this.state.dislikecount - 1 })}><FontAwesomeIcon icon={this.state.dislike} /> {this.state.dislikecount}</Button>
-        </div>
-        );
-    }
+    return (<div className="d-flex justify-content-between">
+        <Button variant="primary" onClick={() => like()}><FontAwesomeIcon icon={state.like} /> {state.likecount}</Button>
+        <Button variant="primary" onClick={() => dislike()}><FontAwesomeIcon icon={state.dislike} /> {state.dislikecount}</Button>
+    </div>
+    );
+
 }
 
 export default Interactor;
