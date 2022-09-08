307d28b43eac8e8a22ae73992e2cdfc61554a122
Dasavanth-Alapati <dasavanth.work@gmail.com>
2022-09-08 19:42:16 +0530

    put api call behind logged in condition

--- src/components/feed.js
+++ src/components/feed.js
@@ -8,21 +8,22 @@ import Post from './post';
 class Feed extends Component {
   state = { feed: null }
   componentDidMount() {
-    fetchfeed().then(res => {
-      this.setState({ feed: res.data });
-    })
+    if (isLoggedIn())
+      fetchfeed().then(res => {
+        this.setState({ feed: res.data });
+      })
   }
 
   render() {
     let posts = []
-    if (isLoggedIn()){
-    if (this.state.feed !== null) {
-      posts = this.state.feed.map((element) => <Post key={element.id} post={element} />)
-      return (<Container className="d-flex justify-content-center"><div className='mt-3'>{posts}</div></Container>);
+    if (isLoggedIn()) {
+      if (this.state.feed !== null) {
+        posts = this.state.feed.map((element) => <Post key={element.id} post={element} />)
+        return (<Container className="d-flex justify-content-center"><div className='mt-3'>{posts}</div></Container>);
+      }
+      else return (<div><Spinner animation='border' variant='primary' size='sm' ></Spinner>Loading...</div>)
     }
-    else return (<div><Spinner animation='border' variant='primary' size='sm' ></Spinner>Loading...</div>)
-  }
-  else return (<><Container>Welcome, please <Link to='/login'>Signin</Link></Container></>);
+    else return (<><Container>Welcome, please <Link to='/login'>Signin</Link></Container></>);
   }
 }
 
