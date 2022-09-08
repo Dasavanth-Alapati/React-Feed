e9323e67ba5d2551f1f17b5774b927d69476a5da
Dasavanth-Alapati <dasavanth.work@gmail.com>
2022-09-08 17:58:17 +0530

    added a small spinner

--- src/components/feed.js
+++ src/components/feed.js
@@ -1,6 +1,6 @@
 import { isLoggedIn } from 'axios-jwt';
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Spinner } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { fetchfeed } from '../services/api';
 import Post from './post';
@@ -20,7 +20,7 @@ class Feed extends Component {
       posts = this.state.feed.map((element) => <Post key={element.id} post={element} />)
       return (<Container className="d-flex justify-content-center"><div className='mt-3'>{posts}</div></Container>);
     }
-    else return (<div>Loading...</div>)
+    else return (<div><Spinner animation='border' variant='primary' size='sm' ></Spinner>Loading...</div>)
   }
   else return (<><Container>Welcome, please <Link to='/login'>Signin</Link></Container></>);
   }
