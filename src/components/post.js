a19e29ac66fce1620297f6a9306afe8ba267c2ef
Dasavanth-Alapati <dasavanth.work@gmail.com>
2022-09-12 16:11:59 +0530

    converted post to match coding conventions

--- src/components/post.js
+++ src/components/post.js
@@ -3,17 +3,17 @@ import Interactor from './interactor';
 import moment from 'moment';
 
 
-function Post(props) {
+const Post = (props) => {
 
   return (
-    <Card style={{  width: "30rem" , marginBottom: '2rem' }}>
+    <Card style={{ width: "30rem", marginBottom: '2rem' }}>
       <Card.Body>
         <Card.Title>{props.post.title}</Card.Title>
         <Card.Text>
           {props.post.content}
         </Card.Text>
-        <Card.Subtitle><small>{props.post.profileid.credid.username}</small></Card.Subtitle><br/>
-        <Interactor like = {props.post.like}></Interactor>
+        <Card.Subtitle><small>{props.post.profileid.credid.username}</small></Card.Subtitle><br />
+        <Interactor like={props.post.like}></Interactor>
       </Card.Body>
       <Card.Footer className="text-muted">{moment(props.post.created_at).fromNow()}</Card.Footer>
     </Card>
